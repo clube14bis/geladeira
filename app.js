2411b386bb4eb024a74ce0a419e4d40db00e0117
Mateus Freitas <mateus.freitas@me.com>
2026-08-24 10:07:25 -0300

    Atualiza versão SF Pro

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ import {
 } from "./catalogo-base.js?v=1.2.4";
 const $ = (s) => document.querySelector(s),
   telas = document.querySelectorAll(".tela"),
-  VERSAO_APP = "V1.5.9",
+  VERSAO_APP = "V1.5.10",
   PIX = "00020126580014BR.GOV.BCB.PIX0136c9cb7e85-240b-46e5-b500-3278442092475204000053039865802BR5912Clube 14 Bis6011Mirassol SP62160512Bebidas14Bis63045F94",
   fmt = (c) =>
     new Intl.NumberFormat("pt-BR", {
